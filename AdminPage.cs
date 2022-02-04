9a95a535b3bd272a152c805cd4273e5490f90956
EminElekberov <eminelekberov09@gmail.com>
2022-02-04 15:44:05 +0400

    first commit

---
+++ AdminPage.cs
@@ -0,0 +1,88 @@
+﻿using LibraryApi.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryApi
+{
+    public partial class AdminPage : Form
+    {
+        private readonly LibraryEntities3 _db;
+        public AdminPage()
+        {
+            InitializeComponent();
+            _db = new LibraryEntities3();
+        }
+
+        private void AdminPage_Load(object sender, EventArgs e)
+        {
+            StudentListDgv.DataSource = _db.Login_User.Where(x => x.IsAdmin == false && x.IsDeleted == false).Select(x => new
+                {
+                    UserId = x.Id,
+                    x.Fullname,
+                    x.Email
+                }).ToList();
+        }
+
+        private void StudentListDgv_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            int userId = int.Parse(StudentListDgv.Rows[e.RowIndex].Cells["UserId"].Value.ToString());
+            Model.Login_User login = _db.Login_User.FirstOrDefault(x => x.Id == userId);
+            txtname.Text = login.Fullname;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            string name = txtname.Text;
+            if (name=="")
+            {
+                MessageBox.Show("Select a User", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            Model.Login_User login_User = _db.Login_User.FirstOrDefault(x => x.Fullname == name);
+            login_User.IsDeleted = true;
+            _db.SaveChanges();
+            MessageBox.Show("Selected Name is Accepted", "Sucess", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            txtname.Text = "";
+            StudentListDgv.DataSource = _db.Login_User.Where(x => x.IsAdmin == false && x.IsDeleted == false).Select(x => new
+            {
+                UserId = x.Id,
+                x.Fullname,
+                x.Email
+            }).ToList();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            string name = txtname.Text;
+            if (name == "")
+            {
+                MessageBox.Show("Select a User", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+            Model.Login_User login_User = _db.Login_User.FirstOrDefault(x => x.Fullname == name);
+            _db.Login_User.Remove(login_User);
+            _db.SaveChanges();
+            MessageBox.Show("Selected Name is Deleted","Sucess",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            txtname.Text = "";
+            StudentListDgv.DataSource = _db.Login_User.Where(x => x.IsAdmin == false && x.IsDeleted == false).Select(x => new
+            {
+                UserId = x.Id,
+                x.Fullname,
+                x.Email
+            }).ToList();
+        }
+
+        private void returnBookToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReturnBook returnBook = new ReturnBook();
+            returnBook.ShowDialog();
+        }
+    }
+}
---
+++ UserOrder.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace LibraryApi
+{
+    partial class UserOrder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserOrder));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbgenre = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSearchname = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buyBookTime = new System.Windows.Forms.DateTimePicker();
+            this.sendBookTime = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SearchPerson = new System.Windows.Forms.PictureBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.SearchBook = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchPerson)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchBook)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-1, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(846, 449);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 9);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Genre";
+            // 
+            // cmbgenre
+            // 
+            this.cmbgenre.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbgenre.FormattingEnabled = true;
+            this.cmbgenre.Location = new System.Drawing.Point(11, 41);
+            this.cmbgenre.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbgenre.Name = "cmbgenre";
+            this.cmbgenre.Size = new System.Drawing.Size(163, 21);
+            this.cmbgenre.TabIndex = 6;
+            this.cmbgenre.SelectedIndexChanged += new System.EventHandler(this.cmbgenre_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(11, 75);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Book Name:";
+            // 
+            // txtSearchname
+            // 
+            this.txtSearchname.Location = new System.Drawing.Point(11, 107);
+            this.txtSearchname.Margin = new System.Windows.Forms.Padding(2);
+            this.txtSearchname.Name = "txtSearchname";
+            this.txtSearchname.Size = new System.Drawing.Size(163, 20);
+            this.txtSearchname.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.Cornsilk;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 147);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(410, 278);
+            this.dataGridView1.TabIndex = 18;
+            this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(478, 6);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 13);
+            this.label4.TabIndex = 23;
+            this.label4.Text = "Give Back";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "Buy date";
+            // 
+            // buyBookTime
+            // 
+            this.buyBookTime.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.buyBookTime.Location = new System.Drawing.Point(287, 25);
+            this.buyBookTime.Name = "buyBookTime";
+            this.buyBookTime.Size = new System.Drawing.Size(103, 20);
+            this.buyBookTime.TabIndex = 21;
+            // 
+            // sendBookTime
+            // 
+            this.sendBookTime.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.sendBookTime.Location = new System.Drawing.Point(481, 25);
+            this.sendBookTime.Name = "sendBookTime";
+            this.sendBookTime.Size = new System.Drawing.Size(112, 20);
+            this.sendBookTime.TabIndex = 24;
+            this.sendBookTime.Value = new System.DateTime(2022, 1, 18, 18, 41, 24, 0);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(425, 75);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(306, 20);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Search person(With personality card)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(420, 107);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(266, 20);
+            this.textBox1.TabIndex = 26;
+            // 
+            // SearchPerson
+            // 
+            this.SearchPerson.BackColor = System.Drawing.Color.Transparent;
+            this.SearchPerson.Image = ((System.Drawing.Image)(resources.GetObject("SearchPerson.Image")));
+            this.SearchPerson.Location = new System.Drawing.Point(712, 98);
+            this.SearchPerson.Name = "SearchPerson";
+            this.SearchPerson.Size = new System.Drawing.Size(57, 31);
+            this.SearchPerson.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SearchPerson.TabIndex = 27;
+            this.SearchPerson.TabStop = false;
+            this.SearchPerson.Click += new System.EventHandler(this.SearchPerson_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(420, 147);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(425, 87);
+            this.dataGridView2.TabIndex = 28;
+            this.dataGridView2.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView2_CellDoubleClick);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(425, 255);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(68, 20);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Orders:";
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Location = new System.Drawing.Point(420, 277);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.RowHeadersWidth = 51;
+            this.dataGridView3.RowTemplate.Height = 24;
+            this.dataGridView3.Size = new System.Drawing.Size(425, 148);
+            this.dataGridView3.TabIndex = 30;
+            // 
+            // SearchBook
+            // 
+            this.SearchBook.BackColor = System.Drawing.Color.Transparent;
+            this.SearchBook.Image = ((System.Drawing.Image)(resources.GetObject("SearchBook.Image")));
+            this.SearchBook.Location = new System.Drawing.Point(188, 96);
+            this.SearchBook.Name = "SearchBook";
+            this.SearchBook.Size = new System.Drawing.Size(57, 31);
+            this.SearchBook.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.SearchBook.TabIndex = 31;
+            this.SearchBook.TabStop = false;
+            this.SearchBook.Click += new System.EventHandler(this.SearchBook_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(638, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(174, 34);
+            this.button1.TabIndex = 32;
+            this.button1.Text = "Buy Book";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // UserOrder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SearchBook);
+            this.Controls.Add(this.dataGridView3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dataGridView2);
+            this.Controls.Add(this.SearchPerson);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.sendBookTime);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buyBookTime);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtSearchname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cmbgenre);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "UserOrder";
+            this.Text = "UserOrder";
+            this.Load += new System.EventHandler(this.UserOrder_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchPerson)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchBook)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbgenre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSearchname;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DateTimePicker buyBookTime;
+        private System.Windows.Forms.DateTimePicker sendBookTime;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox SearchPerson;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.PictureBox SearchBook;
+        private System.Windows.Forms.Button button1;
+    }
+}
